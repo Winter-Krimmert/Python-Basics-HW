aefd7b2333644e2e6c3b9720b7bb547dc62f1ebe
Winter-Krimmert <winterk463@gmail.com>
2024-04-29 14:46:32 -0700

    Python Basics HW

---
+++ Assignment 1.py
@@ -0,0 +1,49 @@
+# Problem 1
+# Task 1 Code Correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")    # correcting the indentation error ensuring that the print statement is indented to show the appropriate code block.
+else:
+    print("Take an umbrella!")   # correcting the indentation error ensuring that the print statement is indented to show the appropriate code block.
+
+# Task 2 Your Mood Today
+
+mood = input("What is your mood today? ")
+if mood == "happy":
+    print("That's great to hear!")
+
+if mood == "sad":
+    print("I hope your day gets better")
+
+# Problem 2
+
+# Task 1 Code Correction
+
+pi_value = 3.14                 # Snake case: words are connected using an underscore, and all letters are lowercase. Constants (variables whose data should never change) require All_UPPER_CASE Snake.
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
+
+
+# Problem 3
+
+# Task 1 Grocery Store Math
+
+#creating a grocery store list
+
+bread = 2.50
+peanut_butter = 3.50
+jelly = 4.00
+
+grocery_store_list = (bread + peanut_butter + jelly)
+
+print(grocery_store_list)
+
+#Task 2 Bank Interest
+
+savings_account = 10000
+interest = 10000*.07
+savings_account_year_1 = savings_account + interest
+print(int(savings_account_year_1))
